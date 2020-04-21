423a18c9fdd71af1b0561472054d7e2ea6730192
Marcel Bader <stu216826@mail.uni-kiel.de>
2020-04-21 20:11:26 +0200

    Added logging.

--- .github/actions/setup-istio/index.js
+++ .github/actions/setup-istio/index.js
@@ -6,16 +6,19 @@ const path = require('path');
 async function installIstio() {
   try {
     // `who-to-greet` input defined in action metadata file
-    console.log(`Installing istio`);
+    console.log(`Downloading istio...`);
     const version = core.getInput('version');
     const os = 'linux'
     const url = `https://github.com/istio/istio/releases/download/${version}/istio-${version}-${os}.tar.gz`
     const downloadIstioScript = await toolCache.downloadTool(url);
-  
+    console.log(`Downloaded istio.`);
+
+    console.log(`Installing istio.`);
     const tempDirectory = path.join('home', 'actions', 'temp');
     await toolCache.extractTar(downloadIstioScript, tempDirectory);
   
     const toolPath = await toolCache.cacheDir(tempDirectory, "istio", version);
+    console.log(`Addingn to path: ${toolPath}`);
     core.addPath(path.join(toolPath, 'bin'));
     core.debug(`istio is cached under ${toolPath}`);
     
