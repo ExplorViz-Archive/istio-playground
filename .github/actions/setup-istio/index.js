deddd2a7757921fc511483968fb2107a6aaf624b
Marcel Bader <stu216826@mail.uni-kiel.de>
2020-04-21 17:01:34 +0200

    Implement istio action.

--- .github/actions/setup-istio/index.js
+++ .github/actions/setup-istio/index.js
@@ -1,15 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const toolCache = require('@actions/tool-cache');
+const exec = require('@actions/exec');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`Installing istio`);
+  const version = '1.5.1'
+  const OSEXT = 'linux'
+  const url = `https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz`
+  const downloadIstioScript = await toolCache.downloadTool(url);
+
+  const tempDirectory = path.join('home', 'actions', 'temp');
+  await toolCache.extractTar(downloadIstioScript, tempDirectory);
+
+  const toolPath = await tc.cacheDir(tempDirectory, "istio", version);
+  core.addPath(cachedPath);
+  core.debug(`istio is cached under ${toolPath}`);
+  core.addPath(path.join(toolPath, 'bin'));
 } catch (error) {
   core.setFailed(error.message);
 }
