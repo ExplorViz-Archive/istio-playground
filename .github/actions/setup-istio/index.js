da17bc7cc4be893ed958a49db5a0d6a1955184f3
Marcel Bader <stu216826@mail.uni-kiel.de>
2020-04-21 20:42:40 +0200

    Fixing.

--- .github/actions/setup-istio/index.js
+++ .github/actions/setup-istio/index.js
@@ -17,8 +17,9 @@ async function installIstio() {
     console.log(`Installing istio.`);
     const tempDirectory = path.join('home', 'actions', 'temp');
     await toolCache.extractTar(downloadIstioScript, tempDirectory);
-  
-    const toolPath = await toolCache.cacheDir(tempDirectory, "istio", version);
+    await exec.exec(`ls -la ${tempDirectory}`);
+
+    const toolPath = await toolCache.cacheDir(tempDirectory);
     const binPath = path.join(toolPath, 'bin');
     console.log(`Adding to path: ${binPath}`);
     core.addPath(binPath);
