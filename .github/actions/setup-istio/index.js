6b7b11b599dd53bdfa7a591aa9adf073f16e7926
Marcel Bader <stu216826@mail.uni-kiel.de>
2020-04-21 17:07:32 +0200

    Add async function.

--- .github/actions/setup-istio/index.js
+++ .github/actions/setup-istio/index.js
@@ -3,21 +3,25 @@ const github = require('@actions/github');
 const toolCache = require('@actions/tool-cache');
 const exec = require('@actions/exec');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  console.log(`Installing istio`);
-  const version = '1.5.1'
-  const OSEXT = 'linux'
-  const url = `https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz`
-  const downloadIstioScript = await toolCache.downloadTool(url);
-
-  const tempDirectory = path.join('home', 'actions', 'temp');
-  await toolCache.extractTar(downloadIstioScript, tempDirectory);
-
-  const toolPath = await tc.cacheDir(tempDirectory, "istio", version);
-  core.addPath(cachedPath);
-  core.debug(`istio is cached under ${toolPath}`);
-  core.addPath(path.join(toolPath, 'bin'));
-} catch (error) {
-  core.setFailed(error.message);
+async function installIstio() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    console.log(`Installing istio`);
+    const version = '1.5.1'
+    const OSEXT = 'linux'
+    const url = `https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-${OSEXT}.tar.gz`
+    const downloadIstioScript = await toolCache.downloadTool(url);
+  
+    const tempDirectory = path.join('home', 'actions', 'temp');
+    await toolCache.extractTar(downloadIstioScript, tempDirectory);
+  
+    const toolPath = await tc.cacheDir(tempDirectory, "istio", version);
+    core.addPath(cachedPath);
+    core.debug(`istio is cached under ${toolPath}`);
+    core.addPath(path.join(toolPath, 'bin'));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+installIstio()
