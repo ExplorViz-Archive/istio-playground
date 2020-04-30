674df0abb24a7357653ff858592a4e82dd9311e6
Marcel Bader <stu216826@mail.uni-kiel.de>
2020-04-30 10:24:03 +0200

    Cleanup. Network resilience.

--- scripts/application-tasks-wrapper.sh
+++ scripts/application-tasks-wrapper.sh
@@ -12,18 +12,45 @@ deploy() {
   echo "Delete old version."
   kubectl delete -f src/test/resources/kube/quarkus/web-app/other-ns-app.yaml
   kubectl delete -f src/test/resources/kube/quarkus/web-app/web-app.yaml
-  kubectl delete -f src/test/resources/kube/quarkus/web-app/rest-app.yaml
+  kubectl delete -f src/test/resources/kube/quarkus/web-app/httpbin-app.yaml
 
   echo "Deploy new version."
   kubectl apply -f src/test/resources/kube/quarkus/web-app/other-ns-app.yaml
   kubectl apply -f src/test/resources/kube/quarkus/web-app/web-app.yaml
-  kubectl apply -f src/test/resources/kube/quarkus/web-app/rest-app.yaml
+  kubectl apply -f src/test/resources/kube/quarkus/web-app/httpbin-app.yaml
 
   INGRESS_HOST=$(minikube ip)
   INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
   echo "##########################################################################"
   echo "Application started: http://$INGRESS_HOST:$INGRESS_PORT/login.html"
   echo "##########################################################################"
+}
+
+apply() {
+  kubectl apply -n $1 -f src/test/resources/kube/$2
+}
+
+demo_simple_authentication() {
+  kubectl create namespace quarkus &
+  kubectl label namespace quarkus istio-injection=enabled
+  apply quarkus quarkus/web-app/web-app.yaml #deploy the demo app
+  apply quarkus quarkus/traffic-management/virtual-service.yaml #make the app accessible via service
+  apply quarkus quarkus/traffic-management/gateway.yaml #setup a gateway
+  apply quarkus quarkus/authentication/authentication-policy.yaml #require authentication for some resources
+}
+
+demo_authorization() {
+  demo_simple_authentication
+  kubectl create namespace foo &
+  kubectl label namespace foo istio-injection=enabled
+  # deploy more applications
+  apply foo quarkus/web-app/web-app.yaml
+  apply foo quarkus/web-app/httpbin.yaml
+  apply quarkus quarkus/web-app/httpbin.yaml
+  apply quarkus quarkus/web-app/web-app-other-versions.yaml
+  apply quarkus quarkus/authorization/deny-all.yaml
+  apply quarkus quarkus/authorization/allow-get-on-web-app.yaml
+  apply quarkus quarkus/authorization/allow-get-from-web-app.yaml
 }
 
 $1
--- src/test/scripts/demo-authorization.sh
+++ src/test/scripts/demo-authorization.sh
@@ -20,38 +20,21 @@ sleep_with_progressbar() {
     printf "]$CHECKMARK\n"
 }
 
-DENY_pattern() {
-    echo "RBAC: access denied"
-}
-
-ALLOW_pattern() {
-    echo "<title>Github auth example</title>"
-}
-
-checkAccessFromOtherNs() {
-    printf "$1 to $2: $3 "
-    pod=$(kubectl get pods -n foo -o jsonpath="{.items[*].metadata.name}" -l app=$1-app)
-    if kubectl exec -n foo "$pod" -c istio-playground -- curl -s http://$2-app-service.quarkus:8000/login.html | grep -q "$($3_pattern)"; then
-        printf "$CHECKMARK\n"
-    else
-        printf "$CROSS\n"
-        exit 1
-    fi
-}
-
 checkAccess() {
-    printf "$1 to $2: $3 "
-    pod=$(kubectl get pods -n quarkus -o jsonpath="{.items[*].metadata.name}" -l app=$1-app)
-    if kubectl exec -n quarkus "$pod" -c istio-playground -- curl -s http://$2-app-service.quarkus:8000/login.html | grep -q "$($3_pattern)"; then
+    printf "$1 to $2: $4 "
+    namespace=$(cut -d "." -f2 <<< "$1")
+    pod=$(kubectl get pods -n $namespace -o jsonpath="{.items[*].metadata.name}" -l curl=able)
+    http_code=$(kubectl exec -n $namespace "$pod" -c istio-playground -- curl -s -o /dev/null -w "%{http_code}" http://$2:8000/$3)
+    if [ $4 = $http_code ]; then
         printf "$CHECKMARK\n"
     else
-        printf "$CROSS\n"
+        printf "$CROSS --> $http_code\n "
         exit 1
     fi
 }
 
 applyPolicy() {
-    kubectl apply -f src/test/resources//kube/quarkus/authorization/${1}.yaml
+    kubectl apply -n quarkus -f src/test/resources//kube/quarkus/authorization/${1}.yaml
     sleep_with_progressbar "Applying policy '$1' " $WAIT_FOR_POLICY_IN_SECONDS
 }
 
@@ -65,40 +48,35 @@ trap "cleanup" EXIT
 
 test_one_for_one() {
     echo "Confirm that all can be accessed:"
-    checkAccess rest web "ALLOW"
-    checkAccess web rest "ALLOW"
-    checkAccessFromOtherNs other-ns rest "ALLOW"
-    checkAccessFromOtherNs other-ns web "ALLOW"
+    checkAccess web.quarkus httpbin.quarkus         ip         200
+    checkAccess web.foo     httpbin.quarkus         ip         200
+    checkAccess web.foo     web-app-service.quarkus login.html 200
 
     applyPolicy deny-all
-    checkAccess rest web "DENY"
-    checkAccess web rest "DENY"
-    checkAccessFromOtherNs other-ns rest "DENY"
-    checkAccessFromOtherNs other-ns web "DENY"
+    checkAccess web.quarkus httpbin.quarkus         ip         403
+    checkAccess web.foo     httpbin.quarkus         ip         403
+    checkAccess web.foo     web-app-service.quarkus login.html 403
 
     applyPolicy allow-get-on-web-app
-    checkAccess rest web "ALLOW"
-    checkAccess web rest "DENY"
-    checkAccessFromOtherNs other-ns rest "DENY"
-    checkAccessFromOtherNs other-ns web "ALLOW"
+    checkAccess web.quarkus httpbin.quarkus         ip         403
+    checkAccess web.foo     httpbin.quarkus         ip         403
+    checkAccess web.foo     web-app-service.quarkus login.html 200
 
     applyPolicy allow-get-from-web-app
-    checkAccess rest web "ALLOW"
-    checkAccess web rest "ALLOW"
-    checkAccessFromOtherNs other-ns rest "DENY"
-    checkAccessFromOtherNs other-ns web "ALLOW"
+    checkAccess web.quarkus httpbin.quarkus         ip         200
+    checkAccess web.foo     httpbin.quarkus         ip         403
+    checkAccess web.foo     web-app-service.quarkus login.html 200
 }
 
 test_all_at_once() {
-    for policy in src/test/resources/kube/quarkus/authorization/*; do
-        kubectl apply -f ${policy}
-    done
+    # for policy in src/test/resources/kube/quarkus/authorization/*; do
+    #     kubectl apply -f ${policy}
+    # done
     sleep_with_progressbar "Waiting for policies to be enabled " $WAIT_FOR_POLICY_IN_SECONDS
-
-    checkAccess rest web "ALLOW"
-    checkAccess web rest "ALLOW"
-    checkAccessFromOtherNs other-ns rest "DENY"
-    checkAccessFromOtherNs other-ns web "ALLOW"
+    #           FROM        TO                      PATH       HTTP_CODE
+    checkAccess web.quarkus httpbin.quarkus         ip         200
+    checkAccess web.foo     httpbin.quarkus         ip         403
+    checkAccess web.foo     web-app-service.quarkus login.html 200
 }
 
 $1
